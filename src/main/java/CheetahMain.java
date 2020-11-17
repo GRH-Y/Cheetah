0d23d4819eb35378f00aacc76441389fb38f8638
ydz <584335070@qq.com>
2020-11-17 17:22:26 +0800

    升级jar包,修改了ui，支持更新功能

--- src/main/java/CheetahMain.java
+++ src/main/java/CheetahMain.java
@@ -1,3 +1,4 @@
+import config.ConfigKey;
 import connect.network.nio.NioClientFactory;
 import connect.network.nio.NioServerFactory;
 import connect.network.xhttp.XHttpConnect;
@@ -6,13 +7,16 @@ import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import task.executor.TaskExecutorPoolManager;
-import ui.controller.ControllerConnect;
+import ui.controller.ControllerMain;
+
+import java.io.File;
 
 public class CheetahMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.getIcons().add(new Image("ic_logo.png"));
-        ControllerConnect.showLoginScene(primaryStage);
+        String dir = System.getProperties().getProperty(ConfigKey.KEY_USER_DIR);
+        primaryStage.getIcons().add(new Image("file:" + dir + File.separator + "ic_logo.png"));
+        ControllerMain.showLoginScene(primaryStage);
         primaryStage.setResizable(false);
         primaryStage.show();
         primaryStage.setOnCloseRequest(event -> {
---
+++ src/main/resources/readme.txt
@@ -0,0 +1,11 @@
+使用说明：
+windows系统双击运行windows.bat，或者双击运行Cheetah.jar
+linux或者mac系统运行linux_mac_start.sh，需要配置可执行权限（chmod 744 linux_mac_start.sh ）
+点击界面连接图标开始连接服务，连接成功标题会显示listener本机地址，浏览器则配置该地址和端口就可以代理访问
+
+
+配置文件说明:
+config.cfg 配置连接参数，数据加密类型，拦截黑名单
+interceptTable.dat 拦截黑名单文件，配置规则见该文件
+private.key 和 public.key 是一套密钥，开启RSA才使用
+proxyTable.dat 代理文件，配置该文件域名强制走代理或者不走代理
