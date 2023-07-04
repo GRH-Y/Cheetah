932ca25911a1466f4cd74451072abf80d027f20e
YySource <12760980+yysource@user.noreply.gitee.com>
2023-07-04 23:25:47 +0800

    [feature] 升级jar包

--- src/main/java/CheetahMain.java
+++ src/main/java/CheetahMain.java
@@ -1,38 +1,9 @@
-import config.ConfigKey;
-import connect.network.nio.NioClientFactory;
-import connect.network.nio.NioServerFactory;
-import connect.network.xhttp.XHttpConnect;
-import intercept.WatchFileManager;
-import javafx.application.Application;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import log.LogDog;
-import task.executor.TaskExecutorPoolManager;
-import ui.controller.ControllerMain;
+import com.open.proxy.ProxyMain;
 
-import java.io.File;
-
-public class CheetahMain extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        String dir = System.getProperties().getProperty(ConfigKey.KEY_USER_DIR);
-        primaryStage.getIcons().add(new Image("file:" + dir + File.separator + "res/ic_logo.png"));
-        LogDog.initLogSavePath(dir + File.separator, "http_proxy");
-        ControllerMain.showLoginScene(primaryStage);
-        primaryStage.setResizable(false);
-        primaryStage.show();
-        primaryStage.setOnCloseRequest(event -> {
-            NioClientFactory.destroy();
-            NioServerFactory.destroy();
-            WatchFileManager.getInstance().destroy();
-            XHttpConnect.destroy();
-            TaskExecutorPoolManager.getInstance().destroyAll();
-        });
-    }
+public class CheetahMain {
 
     public static void main(String[] args) {
-        launch(args);
+        ProxyMain.main(args);
     }
-
 }
 
